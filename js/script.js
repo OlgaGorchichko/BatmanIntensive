ed35fd26c9851155ab06d30651d4e434518bdc54
Olga <olga@gorchichko.com>
2022-02-18 12:01:22 +0300

    working with pagination, added background audio

--- js/script.js
+++ js/script.js
@@ -1,23 +1,80 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
-    loop: true,
-    spaceBetween: 20,
-    slidesPerView: 3,
-    centeredSlidees: true,
-    loopedSlides: 4
+//burger menu
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+    navigation.classList.add('navigation__active')
 });
 
-sliderThumbs.on('click', (swiper) => {
-    swiper.slideTo(swiper.clickedIndex);
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation__active')
 });
 
-const sliderMain = new Swiper('.slider-main', {
-    loop: true,
-    /*thumbs: {
-        swiper: sliderThumbs
-    },*/
-    spaceBetween: 10,
-    loopedSlides: 4
-});
+//music
+try {
+    const mute = document.querySelector('.mute__checkbox');
+    const audio = new Audio('audio/waterTower.mp3');
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+    const checkMute = () => {
+        if (mute.checked) {
+            audio.play().catch(() => {
+                mute.checked = false;
+            });
+        }
+        else {
+            audio.pause();
+        }
+    };
+
+    if (mute) {
+        setTimeout(checkMute, 2000);
+    }
+
+    mute.addEventListener('change', checkMute);
+} catch {
+    console.log('На этой странице нет музыки');
+}
+
+try {
+    const sliderThumbs = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        centeredSlidees: true,
+        loopedSlides: 4
+    });
+
+    sliderThumbs.on('click', (swiper) => {
+        swiper.slideTo(swiper.clickedIndex);
+    });
+
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        /*thumbs: {
+            swiper: sliderThumbs
+        },*/
+        spaceBetween: 10,
+        loopedSlides: 4
+    });
+
+    sliderThumbs.controller.control = sliderMain;
+    sliderMain.controller.control = sliderThumbs;
+
+    const videos = document.querySelectorAll('video');
+
+    sliderMain.on('slideChange', () => {
+        videos.forEach(video => {
+            video.pause();
+        })
+    })
+
+    const pagination = document.querySelector('.pagination');
+    const paginationBtn = document.querySelector('.pagination__arrow');
+
+    paginationBtn.addEventListener('click', () => {
+        pagination.classList.toggle('pagination__active');
+    })
+} catch {
+    console.log('На этой странице нет слайдеров');
+}
